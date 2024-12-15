ec927709e8fc8a56b38e867d29a2994f7e19c535
Girgis Emad <girgisemad74@gmail.com>
2024-12-15 18:00:49 +0200

    divide two modules and handel errors of taking the input

--- beginner/minigrep/src/main.rs
+++ beginner/minigrep/src/main.rs
@@ -1,20 +1,16 @@
-use std::env;
-use std::fs;
-
+use std::{env, process};
+use minigrep::Config;
 fn main() {
-    let args :Vec<String> = env::args().collect();
-    let config =  parse_config(&args);
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
+    match minigrep::run(config) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("Application error : {e}")
+        }
+    }
 }
-
-struct Config {
-    query : String,
-    file_path : String,
-}
-
-fn parse_config(args :&[String] ) -> Config{
-    let query= args[1].clone();
-    let file_path=args[2].clone();
-    Config{query , file_path}
-}
-
