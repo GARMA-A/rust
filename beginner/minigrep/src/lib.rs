c60c75dffc39e60be00bc703cd71a0e554bfa703
Girgis Emad <girgisemad74@gmail.com>
2024-12-17 05:58:56 +0200

    finish CH12 on the book and finish the minigrep small project

--- beginner/minigrep/src/lib.rs
+++ beginner/minigrep/src/lib.rs
@@ -1,12 +1,12 @@
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 
-#[warn(dead_code)]
 pub struct Config {
-    query: String,
-    file_path: String,
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case :bool,
 }
 
- impl Config {
+impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("Not enough arguments");
@@ -14,12 +14,73 @@ pub struct Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config { query, file_path ,ignore_case})
     }
 }
 
+pub fn search_case_sensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results: Vec<&str> = Vec::new();
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
-    println!("With text :\n {content}");
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &content)
+    } else {
+        search_case_sensitive(&config.query, &content)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let content = "\
+Rust :
+safe , fast , productive
+pick the three";
+        assert_eq!(vec!["safe , fast , productive"], search_case_sensitive(query, content));
+    }
+
+    #[test]
+    fn case_insensitive(){
+        let query = "rUsT";
+        let content = "\
+Rust :
+safe , fast , productive
+pick the three
+Trust me.";
+        assert_eq!(vec!["Rust :" , "Trust me."], search_case_insensitive(query, content));
+
+    }
 }
--- beginner/minigrep/src/main.rs
+++ beginner/minigrep/src/main.rs
@@ -1,16 +1,17 @@
-use std::{env, process};
 use minigrep::Config;
+#[warn(dead_code)]
+use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     match minigrep::run(config) {
         Ok(_) => {}
         Err(e) => {
-            println!("Application error : {e}")
+            eprintln!("Application error : {e}")
         }
     }
 }
